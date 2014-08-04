c599fb24b6e4b490a32802bd59d891b460059a52
Pavel <nichitapavel@live.com>
2014-08-04 20:47:44 +0100

    Reorganization of Code!

--- AbstractProjectCode/src/gui/MainWindow.java
+++ AbstractProjectCode/src/gui/MainWindow.java
@@ -3,12 +3,9 @@ package gui;
 import javax.swing.JFrame;
 
 import java.awt.Color;
-import java.awt.Component;
-import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Frame;
-import java.awt.LayoutManager;
 import java.util.ArrayList;
 
 import javax.swing.JTextField;
@@ -18,8 +15,6 @@ import datastructures.AttributeJoint;
 import datastructures.dependency.ADependency;
 import datastructures.dependency.FunctionalDependency;
 import datastructures.dependency.PluralDependency;
-
-import javax.swing.JTextPane;
 
 import java.awt.Font;
 import java.awt.ComponentOrientation;
@@ -36,10 +31,6 @@ import javax.swing.JPanel;
 import javax.swing.JSeparator;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
-import javax.swing.JEditorPane;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import java.awt.Panel;
 import java.awt.BorderLayout;
 
 public class MainWindow {
@@ -48,12 +39,17 @@ public class MainWindow {
 	private ArrayList<ADependency> dependencys;
 	private ArrayList<JCheckBox> antecedentChckBox;
 	private ArrayList<JCheckBox> consecuentChckBox;
-	int i = 0;
+	int iAttr = 0;
+	int iDep = 0;
+	int panelDepSize = 100;
 	
 	private JFrame frmTFG;
 	private JTextField txtAttribute;
 	private JButton btnaddAttribute;
-	private final JPanel panelDep = new JPanel();
+	
+	//paneles
+	JPanel panelAttr = new JPanel();
+	JPanel panelDep = new JPanel();
 
 	/**
 	 * Launch the application.
@@ -89,15 +85,66 @@ public class MainWindow {
 	 * @param frmTFG TODO
 	 */
 	private void initialize(final JFrame frmTFG) {
+		//Inicializar la ventana
 		frmTFG.setExtendedState(Frame.MAXIMIZED_BOTH);
 		frmTFG.setTitle("DBN&U");
 		frmTFG.setBackground(new Color(240, 240, 240));
 		frmTFG.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmTFG.getContentPane().setLayout(null);
 		
-		final JPanel panelAttr = new JPanel();
+		//Poner la barra de menus
+		MenuBar menuBar = new MenuBar();
+		frmTFG.setJMenuBar(menuBar.getMenuBar());
+		
+		//inicializar el panel donde aparecen y se ven los attributos
+		panelAttr.setSize(new Dimension(1920, 150));
+		panelAttr.setLayout(null);
+		
+		//Los elementos del panel de Attributos
+		JLabel txtpnAtributo = new JLabel();
+		txtpnAtributo.setBounds(10, 10, 60, 25);
+		txtpnAtributo.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		txtpnAtributo.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		//txtpnAtributo.setBackground(new Color(240, 240, 240));
+		txtpnAtributo.setText("Atributo:");
 		
 		txtAttribute = new JTextField();		
 		txtAttribute.setBounds(75, 10, 140, 25);
+		txtAttribute.setToolTipText("Escribe el Atributo");
+		txtAttribute.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		txtAttribute.setColumns(10);
+		
+		btnaddAttribute = new JButton("A\u00F1adir Atributo");
+		btnaddAttribute.setBounds(220, 10, 150, 25);
+		btnaddAttribute.setEnabled(false);
+				
+		JLabel lblAntecendente = new JLabel("Antecendente :");
+		lblAntecendente.setBounds(10, 50, 100, 20);
+		lblAntecendente.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		
+		JLabel lblConsecuente = new JLabel("Consecuente  :");
+		lblConsecuente.setBounds(10, 70, 100, 20);
+		lblConsecuente.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		
+		JButton btnAddFuncDep = new JButton("A\u00F1adir Dependencia Funcional");
+		btnAddFuncDep.setBounds(110, 100, 250, 25);
+				
+		JButton btnAddPlurDep = new JButton("A\u00F1adir Dependencia Plural");
+		btnAddPlurDep.setBounds(380, 100, 250, 25);
+
+		//Anadiendo los elementos al panel de atributos
+		panelAttr.add(txtpnAtributo);
+		panelAttr.add(txtAttribute);
+		panelAttr.add(btnaddAttribute);
+		panelAttr.add(lblAntecendente);
+		panelAttr.add(lblConsecuente);
+		panelAttr.add(btnAddFuncDep);
+		panelAttr.add(btnAddPlurDep);
+
+		//añadiendo el panel de attributos a la ventana
+		frmTFG.getContentPane().add(panelAttr, BorderLayout.CENTER);
+		
+		//evento para activar y desactivar el boton de anadir attributos
 		txtAttribute.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyReleased(KeyEvent arg0) {
@@ -107,38 +154,28 @@ public class MainWindow {
 					btnaddAttribute.setEnabled(false);
 			}
 		});
-		txtAttribute.setToolTipText("Escribe el Atributo");
-		txtAttribute.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		txtAttribute.setColumns(10);
 		
-		/*final JList list = new JList();
-		list.setBounds(30, 100, 200, 39);
-		jPanel.add(list);*/
-		
-		btnaddAttribute = new JButton("A\u00F1adir Atributo");
-		btnaddAttribute.setBounds(220, 10, 150, 25);
-		btnaddAttribute.setEnabled(false);
+		//evento al pulsar el boton de anadir attributos
 		btnaddAttribute.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				int width = 130;
-				
 				Attribute attr = new Attribute(txtAttribute.getText());
 				
 				if (!attributes.contains(attr)) {
 					attributes.add(attr);
 					JCheckBox anteChkBox = new JCheckBox(attr.toString()); 
-					anteChkBox.setBounds(width + i * 55, 50, 50, 20);				
+					anteChkBox.setBounds(width + iAttr * 55, 50, 50, 20);				
 					antecedentChckBox.add(anteChkBox);
 					
 					JCheckBox consChkBox = new JCheckBox(attr.toString());
-					consChkBox.setBounds(width + i * 55, 70, 50, 20);
+					consChkBox.setBounds(width + iAttr * 55, 70, 50, 20);
 					consecuentChckBox.add(consChkBox);
 					
 					panelAttr.add(anteChkBox);
 					panelAttr.add(consChkBox);
 					panelAttr.repaint();
 					
-					i++;
+					iAttr++;
 				}
 				else {
 					JOptionPane.showMessageDialog(frmTFG, 
@@ -152,22 +189,12 @@ public class MainWindow {
 				txtAttribute.requestFocus(true);
 			}
 		});
-		
-		MenuBar menuBar = new MenuBar();
-		frmTFG.setJMenuBar(menuBar.getMenuBar());
-		
-		JLabel lblAntecendente = new JLabel("Antecendente :");
-		lblAntecendente.setBounds(10, 50, 100, 20);
-		lblAntecendente.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		
-		JLabel lblConsecuente = new JLabel("Consecuente  :");
-		lblConsecuente.setBounds(10, 70, 100, 20);
-		lblConsecuente.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		
-		JButton btnAddFuncDep = new JButton("A\u00F1adir Dependencia Funcional");
-		btnAddFuncDep.setBounds(110, 100, 250, 25);
+
+		//evento al anadir una dependencia funcional
 		btnAddFuncDep.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				int height = 25;
+
 				AttributeJoint antecendent = new AttributeJoint();
 				AttributeJoint consecuent = new AttributeJoint();
 				for (JCheckBox chkBoxAttr : antecedentChckBox)
@@ -182,12 +209,34 @@ public class MainWindow {
 						chkBoxAttr.setSelected(false);
 					}
 				
-				dependencys.add(new FunctionalDependency(antecendent, consecuent));
+				ADependency newDep = new FunctionalDependency(antecendent, consecuent);
+				
+				if (!dependencys.contains(newDep)) {
+					dependencys.add(newDep);
+					JCheckBox dfChkBox = new JCheckBox(newDep.toString()); 
+					dfChkBox.setBounds(50, height + iDep * 25, 100, 20);				
+					antecedentChckBox.add(dfChkBox);
+					
+					panelDep.add(dfChkBox);
+					panelDep.setPreferredSize(new Dimension(1920, panelDepSize+height*iDep));
+					frmTFG.repaint();
+					panelDep.repaint();
+					
+					dependencys.add(new FunctionalDependency(antecendent, consecuent));
+					
+					iDep++;
+				}
+				else {
+					JOptionPane.showMessageDialog(frmTFG, 
+							"La dependencia funcional \"" + newDep + "\" ya existe!", 
+							"Duplicidad de dependencias", 
+							JOptionPane.INFORMATION_MESSAGE);
+					newDep = null;
+				}
 			}
 		});
-		
-		JButton btnAddPlurDep = new JButton("A\u00F1adir Dependencia Plural");
-		btnAddPlurDep.setBounds(380, 100, 250, 25);
+
+		//evento al anadir una dependencias plural
 		btnAddPlurDep.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				AttributeJoint antecendent = new AttributeJoint();
@@ -207,37 +256,23 @@ public class MainWindow {
 				dependencys.add(new PluralDependency(antecendent, consecuent));
 			}
 		});
-
-		JTextPane txtpnAtributo = new JTextPane();
-		txtpnAtributo.setBounds(10, 10, 60, 25);
-		txtpnAtributo.setEditable(false);
-		txtpnAtributo.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
-		txtpnAtributo.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		txtpnAtributo.setBackground(new Color(240, 240, 240));
-		txtpnAtributo.setText("Atributo:");
 		
-		panelAttr.setLayout(null);
-		panelAttr.add(txtpnAtributo);
-		panelAttr.add(txtAttribute);
-		panelAttr.add(btnaddAttribute);
-		panelAttr.add(lblAntecendente);
-		panelAttr.add(lblConsecuente);
-		panelAttr.add(btnAddFuncDep);
-		panelAttr.add(btnAddPlurDep);
+		//inicializar el panel de las dependencias
+		panelDep.setSize(new Dimension(1920, panelDepSize));
+		panelDep.setLocation(0, 150);
+		//panelDep.setBackground(new Color(150, 90, 200));
+		panelDep.setLayout(null);
 		
-		frmTFG.getContentPane().add(panelAttr);
-		
+		//
 		JSeparator separator = new JSeparator();
-		separator.setBounds(0, 150, 1920, 1);
-		panelAttr.add(separator);
+		separator.setBounds(0, 0, 1920, 1);
 		
+		//
+		panelDep.add(separator);
 		
-		JCheckBox chckbxNewCheckBox = new JCheckBox("New check box");
-		chckbxNewCheckBox.setBounds(10, 178, 97, 23);
-		panelDep.setBounds(0, 163, 434, 66);
-		panelAttr.add(panelDep);
+		//anadir el panel de dependencias a la ventana
+		frmTFG.getContentPane().add(panelDep, BorderLayout.CENTER);
 		
-		panelDep.add(chckbxNewCheckBox);
 	}
 	
 	public JFrame getFrmTrabajoFinDe() {
